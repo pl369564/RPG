897772f7c0bc7461f48247a53eea06f42e81b1db
MoSAro <pl369564@gmail.com>
2018-04-20 17:15:23 +0800

    Add files via upload

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    string str = string.Empty;
+
+#if UNITY_ANDROID
+        AndroidJavaClass ajc = new AndroidJavaClass("SDK");
+        str = ajc.Call<string>("method");  
+#elif UNITY_IOS
+        
+#endif
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
